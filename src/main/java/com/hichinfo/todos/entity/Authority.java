2e4269912eb9ed8b233891eb74c2444da6fc49a3
Amine Hichri <amyn.hichri@gmail.com>
2025-09-12 14:44:17 +0200

    My First commit - here we gooo

---
+++ src/main/java/com/hichinfo/todos/entity/Authority.java
@@ -0,0 +1,21 @@
+package com.hichinfo.todos.entity;
+
+import jakarta.persistence.Embeddable;
+import org.springframework.security.core.GrantedAuthority;
+
+@Embeddable
+public class Authority implements GrantedAuthority {
+
+    private String authority;
+
+    private Authority() {}
+
+    public Authority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public String getAuthority() {
+        return null;
+    }
+}
