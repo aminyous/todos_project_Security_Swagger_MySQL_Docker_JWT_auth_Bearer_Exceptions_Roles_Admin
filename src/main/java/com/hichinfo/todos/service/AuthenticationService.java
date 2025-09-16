ee669eb20a9e367b67100370e6a99fdf72e52870
Amine Hichri <amyn.hichri@gmail.com>
2025-09-16 14:25:58 +0200

    Login - Authentication service

--- src/main/java/com/hichinfo/todos/service/AuthenticationService.java
+++ src/main/java/com/hichinfo/todos/service/AuthenticationService.java
@@ -1,7 +1,10 @@
 package com.hichinfo.todos.service;
 
+import com.hichinfo.todos.request.AuthenticationRequest;
 import com.hichinfo.todos.request.RegisterRequest;
+import com.hichinfo.todos.response.AuthenticationResponse;
 
 public interface AuthenticationService {
     void register(RegisterRequest input) throws Exception;
+    AuthenticationResponse login(AuthenticationRequest request);
 }
