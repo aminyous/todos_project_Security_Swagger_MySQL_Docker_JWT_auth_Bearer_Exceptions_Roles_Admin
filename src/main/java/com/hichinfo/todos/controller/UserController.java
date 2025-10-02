486e8f0e61c538385620a615dce629323608a938
Amine Hichri <amyn.hichri@gmail.com>
2025-10-02 15:46:34 +0200

    Update password - Tags and status

--- src/main/java/com/hichinfo/todos/controller/UserController.java
+++ src/main/java/com/hichinfo/todos/controller/UserController.java
@@ -4,8 +4,10 @@ import com.hichinfo.todos.entity.User;
 import com.hichinfo.todos.request.PasswordUpdateRequest;
 import com.hichinfo.todos.response.UserResponse;
 import com.hichinfo.todos.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.nio.file.AccessDeniedException;
@@ -22,16 +24,22 @@ public class UserController {
     }
 
     @GetMapping("/info")
+    @Operation(summary = "User information", description = "Get current user info.")
+    @ResponseStatus(HttpStatus.OK)
     public UserResponse getUserInfo() throws AccessDeniedException {
         return userService.getUserInfo();
     }
 
     @DeleteMapping
+    @Operation(summary = "Delete user", description = "Delete current user account.")
+    @ResponseStatus(HttpStatus.OK)
     public void deleteUser() throws AccessDeniedException {
         userService.deleteUser();
     }
 
     @PutMapping("/password")
+    @Operation(summary = "Password update", description = "Change user password after verification.")
+    @ResponseStatus(HttpStatus.OK)
     public void passwordUpdate(@Valid @RequestBody PasswordUpdateRequest passwordUpdateRequest) throws AccessDeniedException {
         userService.updatePassword(passwordUpdateRequest);
 
